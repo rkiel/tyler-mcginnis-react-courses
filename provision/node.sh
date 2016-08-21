3fc4f1f8796c486e6d53bb8ffd3b6991d57268c6
rkiel <cygnus@pobox.com>
2016-08-20 20:40:42 -0400

    added Vagrant

---
+++ provision/node.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "Installing node"
+
+echo "  updating"
+apt-get update >/dev/null 2>&1
+
+echo "setup for Node 4"
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+
+echo "  installing node.js"
+apt-get install -y nodejs >/dev/null 2>&1
+
+echo "  installing npm"
+apt-get install -y npm >/dev/null 2>&1
+
+echo "  installing gcc make build-essential"
+apt-get install -y gcc make build-essential
+
+echo "  purging npm modules"
+rm -rf ~/.npm /vagrant/node_modules
+
+echo "node installed"
